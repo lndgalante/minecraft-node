aa53171245a5963191b07c380b993d8608686716
Leonardo Galante <fenwil@fenwil.local>
2016-11-24 05:10:50 -0300

    v 0.0.2

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const minecraftServer = new MinecraftServer()
 
 minecraftServer.readSetup()
 
-// minecraftServer.welcome()
-// minecraftServer.runSetup()
-// minecraftServer.showSetup()
-// minecraftServer.acceptAgreement()
-// minecraftServer.launchServer()
-// minecraftServer.showIp()
-
-// Minecraft Version 1.11 - 11/14/16
+// minecraftServer.readProperties() minecraftServer.welcome()
+// minecraftServer.runSetup() minecraftServer.showSetup()
+// minecraftServer.acceptAgreement() minecraftServer.launchServer()
+// minecraftServer.showIp() Minecraft Version 1.11 - 11/14/16 eula.txt must set
+// eula=true in order to accept the agreement and launch the server online-mode
+// in server.properties must set to false in order to try LAN save .json to java
+// .properties make a server-properties.json
--- minecraft-sv.js
+++ minecraft-sv.js
@@ -5,13 +5,106 @@ module.exports =
       this.exec = require('child_process').exec
       this.fs = require('fs')
       this.jsonfile = require('jsonfile')
+      this.propertiesReader = require('properties-reader')
       this.publicIp = require('public-ip')
       this.minimumRamMb = 0
       this.maximumRamMb = 0
       this.serverFileName = ''
       this.gui = ''
       this.command = ''
-      this.setupFile = 'setup.json'
+      this.serverSetupFile = 'server-setup.json'
+      // Server Properties
+      this.serverPropertiesFile = 'server-properties.json'
+      this.properties = this.propertiesReader('server.properties')
+      this.maxTickTime = 0
+      this.generatorSettings = ''
+      this.forceGameMode = ''
+      this.allowNether = ''
+      this.gameMode = 0
+      this.enableQuery = false
+      this.playerIdleTimeout = 0
+      this.difficulty = 1
+      this.spawnMonsters = true
+      this.opPermissionLevel = 4
+      this.announcePlayerAchievements = true
+      this.pvp = true
+      this.snooperEnabled = true
+      this.levelType = 'default'
+      this.hardcore = false
+      this.enableCommandBlock = false
+      this.maxPlayers = 20
+      this.networkCompressionThreshold = 256
+      this.resourcePackSha1 = ''
+      this.maxWorldSize = 29999984
+      this.serverPort = 25565
+      this.serverIp = ''
+      this.spawnNpcs = true
+      this.allowFlight = false
+      this.levelName = 'world'
+      this.viewDistance = 10
+      this.resourcePack = ''
+      this.spawnAnimals = true
+      this.whiteList = false
+      this.generateStructures = true
+      this.onlineMode = false
+      this.maxBuildHeight = 256
+      this.levelSeed = ''
+      this.preventProxyConnections = false
+      this.enableRcon = false
+      this.motd = 'A minecraft server'
+      this.readProperties()
+    }
+    showProperties () {
+      console.log('------------------------------------------------')
+      console.log('This are your properties: ')
+      console.log(`Server name: ${this.serverName}`)
+      console.log(`Online mode: ${this.onlineMode}`)
+      console.log(`Server IP: ${this.serverIp}`)
+      console.log(`Server port: ${this.serverPort}`)
+      console.log(`Maximum players: ${this.maxPlayers}`)
+    }
+    readProperties () {
+      this.serverName = this.properties.get('motd')
+      this.onlineMode = this.properties.get('online-mode')
+      this.serverIp = this.properties.get('server-ip')
+      this.serverPort = this.properties.get('server-port')
+      this.maxPlayers = this.properties.get('max-players')
+    }
+    modifyProperties () {
+      console.log('1) Configure your server properties')
+
+      this.serverName = this.readlineSync.question('Insert your server name: ')
+
+      console.log('Want to play LAN as well?')
+
+      let selected = ['YES', 'NO']
+
+      let index = this.readlineSync.keyInSelect(selected, 'Choose an option', {
+        cancel: false
+      })
+      if (selected[index] === 'YES') {
+        this.onlineMode = false
+      } else if (selected[index] === 'NO') {
+        this.onlineMode = true
+      }
+
+      this.serverIp = this.readlineSync.question('Insert server IP: ')
+
+      this.serverPort = this.readlineSync.question('Insert server port: ')
+
+      this.maxPlayers = this.readlineSync.question('Insert maximum players: ')
+    }
+    writeProperties () {
+      this.properties.set('motd', this.serverName)
+      this.properties.set('online-mode', this.onlineMode)
+      this.properties.set('server-ip', this.serverIp)
+      this.properties.set('server-port', this.serverPort)
+      this.properties.set('max-players', this.maxPlayers)
+      this.properties.save('server.properties', (err) => {
+        if (err)
+          throw errs
+        console.log("It's saved!")
+      })
     }
     welcome () {
       console.log('Hi this is the setup manager for your Minecraft Server!')
@@ -19,70 +112,86 @@ module.exports =
       console.log('------------------------------------------------')
     }
     readSetup () {
-      this.fs.open(this.setupFile, 'r', (err, fd) => {
+      this.fs.open(this.serverSetupFile, 'r', (err, fd) => {
         if (err) {
           if (err.code === 'ENOENT') {
             this.welcome()
-            this.runSetup()
+            this.modifySetup()
             this.showSetup()
             return
           } else {
             throw err
           }
         }
+
+        // Read Setup
         console.log('You already have a configuration file: ')
-        this.minimumRamMb = this.jsonfile.readFileSync(this.setupFile).minimumRamMb
-        this.maximumRamMb = this.jsonfile.readFileSync(this.setupFile).maximumRamMb
-        this.serverFileName = this.jsonfile.readFileSync(this.setupFile).serverFileName
-        this.gui = this.jsonfile.readFileSync(this.setupFile).gui
+        this.minimumRamMb = this.jsonfile.readFileSync(this.serverSetupFile).minimumRamMb
+        this.maximumRamMb = this.jsonfile.readFileSync(this.serverSetupFile).maximumRamMb
+        this.serverFileName = this.jsonfile.readFileSync(this.serverSetupFile).serverFileName
+        this.gui = this.jsonfile.readFileSync(this.serverSetupFile).gui
+
+        // Show Setup
         console.log(`Minimum Ram: ${this.minimumRamMb}MB`)
         console.log(`Maximum Ram: ${this.maximumRamMb}MB`)
         console.log(`Server file: ${this.serverFileName}`)
-        ;(this.gui === 'nogui') ?
-          console.log('GUI: Deactivated') :
-          console.log('GUI: Activated')
+        ;(this.gui === 'nogui') ? console.log('GUI: Deactivated') : console.log('GUI: Activated')
+
+        this.showProperties()
         this.menu()
       })
     }
-    runSetup () {
-      console.log('1) Configure your server options')
+    modifySetup () {
+      console.log('Configure your server options')
       this.minimumRamMb = this.readlineSync.question('Insert the minimum RAM in MB: ')
       this.maximumRamMb = this.readlineSync.question('Insert the maximum RAM in MB: ')
       this.serverFileName = this.readlineSync.question('Insert the server file name: ') + '.jar'
       console.log('Want to launch server with a GUI?')
       let selected = ['YES', 'NO']
-      let index = this.readlineSync.keyInSelect(selected, 'Choose an option', {cancel: false})
+      let index = this.readlineSync.keyInSelect(selected, 'Choose an option', {
+        cancel: false
+      })
       if (selected[index] === 'YES') {
         this.gui = 'gui'
       } else if (selected[index] === 'NO') {
         this.gui = 'nogui'
       }
-      this.jsonfile.writeFileSync(this.setupFile, {
+      this.jsonfile.writeFileSync(this.serverSetupFile, {
         minimumRamMb: this.minimumRamMb,
         maximumRamMb: this.maximumRamMb,
         serverFileName: this.serverFileName,
         gui: this.gui
       })
     }
-    menu () {
-      let selected = ['RUN SERVER', 'MODIFY']
-      let index = this.readlineSync.keyInSelect(selected, 'Choose an option')
-      if (selected[index] === 'RUN SERVER') {
-        this.launchServer()
-      } else if (selected[index] === 'MODIFY') {
-        this.runSetup()
-        this.showSetup()
-      }}
     showSetup () {
       console.log('------------------------------------------------')
-      console.log('2) This is your setup')
+      console.log('This is your setup')
       console.log(`Minimum Ram: ${this.minimumRamMb}MB`)
       console.log(`Maximum Ram: ${this.maximumRamMb}MB`)
       console.log(`Server file: ${this.serverFileName}`)
-      ;(this.gui === 'nogui') ?
-        console.log('GUI: Deactivated') :
-        console.log('GUI: Activated')
+      ;(this.gui === 'nogui') ? console.log('GUI: Deactivated') : console.log('GUI: Activated')
+      console.log('------------------------------------------------')
+      console.log('This are your properties: ')
+      console.log(`Server name: ${this.serverName}`)
+      console.log(`Online mode: ${this.onlineMode}`)
+      console.log(`Server IP: ${this.serverIp}`)
+      console.log(`Server port: ${this.serverPort}`)
+      console.log(`Maximum players: ${this.maxPlayers}`)
       this.menu()
+    }
+    menu () {
+      let selected = ['LAUNCH SERVER', 'MODIFY SETUP', 'MODIFY PROPERTIES']
+      let index = this.readlineSync.keyInSelect(selected, 'Choose an option')
+      if (selected[index] === 'LAUNCH SERVER') {
+        this.launchServer()
+      } else if (selected[index] === 'MODIFY SETUP') {
+        this.modifySetup()
+        this.showSetup()
+      }else if (selected[index] === 'MODIFY PROPERTIES') {
+        this.modifyProperties()
+        this.writeProperties()
+        this.showSetup()
+      }
     }
     launchServer () {
       this.command = 'java -Xms' + this.minimumRamMb + 'M -Xmx' + this.maximumRamMb + 'M -jar ' + this.serverFileName + ' ' + this.gui
