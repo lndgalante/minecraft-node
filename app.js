cfd9b45a20d86c10fa8315cd9758082361d11070
Leonardo Galante <fenwil@MacBook-Pro.local>
2016-11-28 04:04:59 -0300

    comments removed

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
 const MinecraftServer = require('./class/MinecraftServer')
 const minecraftServer = new MinecraftServer()
-
-// const Agreement = require('./class/Agreement')
-// const agreement = new Agreement()
-
-// const Properties = require('./class/Properties')
-// const properties = new Properties()
-
-// const Setup = require('./class/Setup')
-// const setup = new Setup()
-
-// agreement.writeEula()
-console.log('')
-// properties.menu()
-console.log('')
 minecraftServer.menu()
-
-// minecraftServer.readSetup()
-
-// minecraftServer.readProperties() minecraftServer.welcome()
-// minecraftServer.runSetup() minecraftServer.showSetup()
-// minecraftServer.acceptAgreement() minecraftServer.launchServer()
-// minecraftServer.showIp() Minecraft Version 1.11 - 11/14/16 eula.txt must set
-// eula=true in order to accept the agreement and launch the server online-mode
-// in server.properties must set to false in order to try LAN save .json to java
-// .properties make a server-properties.json
